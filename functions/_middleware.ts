d714220a5596f2428c27a7cc51f41831513c6603
s-schmidbauer <stefan@schmidbauer.cz>
2024-04-22 18:51:14 +0200

    add country-based redirects v1

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -18,3 +18,30 @@ export const onRequest: PagesFunction = async (context) => {
   response.headers.set('Access-Control-Max-Age', '86400');
   return response;
 };
+
+// redirect default fetch based on country
+export default {
+  async fetch(request) {
+    /**
+     * A map of the URLs to redirect to
+     * @param {Object} countryMap
+     */
+    const countryMap = {
+      DE: "https://www.schmidbauer.cz/de/",
+      AT: "https://www.schmidbauer.cz/de/",
+      CH: "https://www.schmidbauer.cz/de/",
+      CZ: "https://www.schmidbauer.cz/cz/",
+    };
+
+    // Use the cf object to obtain the country of the request
+    // more on the cf object: https://developers.cloudflare.com/workers/runtime-apis/request#incomingrequestcfproperties
+    const country = request.cf.country;
+
+    if (country != null && country in countryMap) {
+      const url = countryMap[country];
+      return Response.redirect(url);
+    } else {
+      return fetch(request);
+    }
+  },
+} satisfies ExportedHandler;
