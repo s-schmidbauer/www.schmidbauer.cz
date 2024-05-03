3e62fe21ab3c9627ecbb894b76623d0facae32a3
s-schmidbauer <stefan@schmidbauer.cz>
2024-05-03 13:41:01 +0200

    fix code problems

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -16,7 +16,7 @@ interface Env {
   VIEWS: KVNamespace;
 }
 
-function logRequest() {
+async function logRequest() {
   const now = Date.now();
   const clientIP = request.headers.get('CF-Connecting-IP');
   const output = `{ "time": "${now}", "clientIP": "${clientIP}", "asn": "${request.cf.asn}", "country": "${request.cf.country}", "region": "${request.cf.region}", "city": "${request.cf.city}", "tlsCipher": "${request.cf.tlsCipher}", "tlsVersion": "${request.cf.tlsVersion}" }`;
